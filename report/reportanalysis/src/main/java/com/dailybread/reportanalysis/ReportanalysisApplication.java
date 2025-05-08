b8e3d799250452a5ecf79cbc79cfa7630273127b
Kinzang Dorji <kinzasdorji66@gmail.com>
2025-05-08 22:24:05 +0000

    fixed eureka issues

--- report/reportanalysis/src/main/java/com/dailybread/reportanalysis/ReportanalysisApplication.java
+++ report/reportanalysis/src/main/java/com/dailybread/reportanalysis/ReportanalysisApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ReportanalysisApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ReportanalysisApplication.class, args);
 	}
